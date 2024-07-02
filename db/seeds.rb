0c087f97147a3120d773741611390a46875b943c
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2024-07-02 21:56:27 +0900

    Restaurant stories complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "chinese"}
+saizeriya = {name: "Saizeriya", address: "Tokyo", category: "italian"}
+taco_rico = {name: "Taco Rico", address: "Tokyo", category: "french"}
+taco_rico_2 = {name: "Taco Rico 2", address: "Tokyo", category: "french"}
+
+
+
+[dishoom, pizza_east, saizeriya, taco_rico, taco_rico_2].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
